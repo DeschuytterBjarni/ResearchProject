cce78ad95a7607bb18282624d9023befec02cd34
DeschuytterBjarni <bjarni.deschuytter@student.howest.be>
2024-01-11 15:46:19 +0100

    feat: stripe payment

--- RPshop/src/environments/environment.ts
+++ RPshop/src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    stripe: {
+        publicKey: 'pk_test_51OXMyXBAGXxtjoebM9M8wpjdIxljqKQajqk4zczFV1DgcTLFJ2Lb1okLPYqaxQAawKFlGsXyiaDHWrJ8pC3vjD4X00pn8qAbYm',
+    }
+};
