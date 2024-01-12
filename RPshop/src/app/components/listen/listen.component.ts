0cffa115c63ab05692c524a24d1ffa927c0bf239
DeschuytterBjarni <bjarni.deschuytter@student.howest.be>
2024-01-12 15:57:49 +0100

    chore: deleted comments & logs

--- RPshop/src/app/components/listen/listen.component.ts
+++ RPshop/src/app/components/listen/listen.component.ts
@@ -9,14 +9,7 @@ import { filter, map } from 'rxjs/operators';
   templateUrl: './listen.component.html'
 })
 export class ListenComponent implements OnInit, OnDestroy {
-  nouns: string[] | undefined;
-  verbs: string[] | undefined;
-  adjs: string[] | undefined;
-  subs: Subscription | undefined;
-  testListen: Subscription | undefined;
-  nounSub: Subscription | undefined;
-  verbSub: Subscription | undefined;
-  adjSub: Subscription | undefined;
+  listen: Subscription | undefined;
   errorsSub: Subscription | undefined;
   errorMsg: any;
   subscription: any;
@@ -26,52 +19,12 @@ export class ListenComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.speech.init();
     this._listen();
-    // this._listenNouns();
-    // this._listenVerbs();
-    // this._listenAdj();
     this._listenErrors();
   }
 
   get btnLabel(): string {
     return this.speech.listening ? 'Listening...' : 'Listen';
   }
-
-  // private _listenNouns() {
-  // this.testListen = this.speech.words$
-  // console.log(this.testListen);
-  // .filter((obj: { type: string; }) => obj.type === 'noun')
-  // .map((nounObj: { word: string; }) => nounObj.word)
-  // .subscribe(
-  //   (noun: string) => {
-  //     this._setError();
-  //     console.log('noun:', noun);
-  //   }
-  // );
-  // }
-
-  // private _listenVerbs() {
-  //   this.verbSub = this.speech.words$
-  //     .filter((obj: { type: string; }) => obj.type === 'verb')
-  //     .map((verbObj: { word: any; }) => verbObj.word)
-  //     .subscribe(
-  //       (verb: any) => {
-  //         this._setError();
-  //         console.log('verb:', verb);
-  //       }
-  //     );
-  // }
-
-  // private _listenAdj() {
-  //   this.adjSub = this.speech.words$
-  //     .filter((obj: { type: string; }) => obj.type === 'adj')
-  //     .map((adjObj: { word: any; }) => adjObj.word)
-  //     .subscribe(
-  //       (adj: any) => {
-  //         this._setError();
-  //         console.log('adjective:', adj);
-  //       }
-  //     );
-  // }
 
   private _listenErrors() {
     this.errorsSub = this.speech.errors$
@@ -88,7 +41,7 @@ export class ListenComponent implements OnInit, OnDestroy {
   }
 
   private _listen() {
-    this.testListen = this.speech.string$.subscribe(text => this._setString(text));
+    this.listen = this.speech.string$.subscribe(text => this._setString(text));
   }
 
   private _setString(text: string) {
@@ -101,20 +54,7 @@ export class ListenComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    if (this.nounSub) {
-      this.nounSub.unsubscribe();
-    }
-    if (this.verbSub) {
-      this.verbSub.unsubscribe();
-    }
-    if (this.adjSub) {
-      this.adjSub.unsubscribe();
-    }
-    if (this.errorsSub) {
-      this.errorsSub.unsubscribe();
-    }
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.errorsSub?.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 }
